707cee017a290838ade603372f6b6e18bb320873
phcavelar <9154669+phcavelar@users.noreply.github.com>
2023-06-01 15:01:45 +0800

    Initial naive sampling with uncond/cond 2d AE+VAE

---
+++ cond_vae_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+env_name='cond_vae'
+
+# Prefer mamba if possible
+if command -v mamba; then
+    install_method='mamba'
+else
+    install_method='conda'
+fi
+
+if command -v nvidia-smi; then
+    if nvidia-smi | grep 'CUDA Version: 11.7'; then
+        env_postfix='_cu11.7'
+    elif nvidia-smi | grep 'CUDA Version: 11.[89]' || nvidia-smi | grep 'CUDA Version: 1[23456789]'; then
+        env_postfix='_cu11.8'
+    else
+        env_postfix='_cpu'
+    fi
+else
+    env_postfix='_cpu'
+fi
+
+
+conda create -n ${env_name}
+command ${install_method} env update -n ${env_name} --file ${env_name}${env_postfix}.yml
